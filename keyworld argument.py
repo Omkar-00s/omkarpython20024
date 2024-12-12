1ffdeea1d24fb6c352baafd7d120a1283cc52de0
Omkar-00s <omkarmomkarm842@gmail.com>
2024-12-12 02:53:46 -0800

    Create keyworld argument.py

---
+++ keyworld argument.py
@@ -0,0 +1,16 @@
+#key world arrgement
+def greet(name):
+  print("Hello," +name+"!")
+
+greet("alice")
+
+def add(a,b):
+  return a+b
+
+
+print(add(5,3))
+
+def great(name, message):
+  print(message + "," + name + "!")
+
+great(name="Alice", message="good morning")
